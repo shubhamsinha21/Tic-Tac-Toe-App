50851511158b147282860f288279e942d1723ac8
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-21 12:53:40 +0530

    flatlist added ````

--- src/App.tsx
+++ src/App.tsx
@@ -1,147 +1,155 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
+  FlatList,
   View,
+  Pressable,
 } from 'react-native';
 
 import Snackbar from 'react-native-snackbar';
 import Icons from './components/Icons';
 
 function App(): JSX.Element {
-
-  const [isCross, setIsCross] = useState<boolean>(false)
-  const [gameWinner, setGameWinner] = useState<string>('')
-  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9))
+  const [isCross, setIsCross] = useState<boolean>(false);
+  const [gameWinner, setGameWinner] = useState<string>('');
+  const [gameState, setGameState] = useState(new Array(9).fill('empty', 0, 9));
 
   const reloadGame = () => {
-    setIsCross(false)
-    setGameWinner('')
-    setGameState(new Array(9).fill('empty', 0, 9))
-  }
+    setIsCross(false);
+    setGameWinner('');
+    setGameState(new Array(9).fill('empty', 0, 9));
+  };
 
   const checkIsWinner = () => {
     // checking winner of game
-    if(
+    if (
       gameState[0] !== 'empty' &&
       gameState[0] === gameState[1] &&
       gameState[1] === gameState[2]
     ) {
-      setGameWinner(`${gameState[0]}won the game ! 😊`)
-    }
-    else if(
+      setGameWinner(`${gameState[0]}won the game ! 😊`);
+    } else if (
       gameState[3] !== 'empty' &&
       gameState[3] === gameState[4] &&
       gameState[4] === gameState[5]
-    ){
-      setGameWinner(`${gameState[3]}won the game ! 😊`)
-    }
-    else if(
+    ) {
+      setGameWinner(`${gameState[3]}won the game ! 😊`);
+    } else if (
       gameState[6] !== 'empty' &&
       gameState[6] === gameState[7] &&
       gameState[7] === gameState[8]
-    ){
-      setGameWinner(`${gameState[6]}won the game ! 😊`)
-    }
-    else if(
+    ) {
+      setGameWinner(`${gameState[6]}won the game ! 😊`);
+    } else if (
       gameState[0] !== 'empty' &&
       gameState[0] === gameState[3] &&
       gameState[3] === gameState[6]
-    ){
-      setGameWinner(`${gameState[0]}won the game ! 😊`)
-    }
-    else if(
+    ) {
+      setGameWinner(`${gameState[0]}won the game ! 😊`);
+    } else if (
       gameState[1] !== 'empty' &&
       gameState[1] === gameState[4] &&
       gameState[4] === gameState[7]
-    ){
-      setGameWinner(`${gameState[1]}won the game ! 😊`)
-    }
-    else if(
+    ) {
+      setGameWinner(`${gameState[1]}won the game ! 😊`);
+    } else if (
       gameState[2] !== 'empty' &&
       gameState[2] === gameState[5] &&
       gameState[5] === gameState[8]
-    ){
-      setGameWinner(`${gameState[2]}won the game ! 😊`)
-    }
-    else if(
+    ) {
+      setGameWinner(`${gameState[2]}won the game ! 😊`);
+    } else if (
       gameState[0] !== 'empty' &&
       gameState[0] === gameState[4] &&
       gameState[4] === gameState[8]
-    ){
-      setGameWinner(`${gameState[0]}won the game ! 😊`)
-    }
-    else if(
+    ) {
+      setGameWinner(`${gameState[0]}won the game ! 😊`);
+    } else if (
       gameState[2] !== 'empty' &&
       gameState[2] === gameState[4] &&
       gameState[4] === gameState[6]
-    ){
-      setGameWinner(`${gameState[2]}won the game ! 😊`)
+    ) {
+      setGameWinner(`${gameState[2]}won the game ! 😊`);
+    } else if (!gameState.includes('empty', 0)) {
+      setGameWinner('Draw game ... 🤛');
     }
-    else if(
-      !gameState.includes('empty',0)
-    )
-    {
-      setGameWinner('Draw game ... 🤛')
-    }
-  
-  }
-    
-  const onChangeItem = (itemNumber:number) =>{
-    if(gameWinner) {
+  };
+
+  const onChangeItem = (itemNumber: number) => {
+    if (gameWinner) {
       return Snackbar.show({
-        text:'gameWinner',
-        backgroundColor:'#000000',
-        textColor:'#FFFFFF'
-      })
+        text: 'gameWinner',
+        backgroundColor: '#000000',
+        textColor: '#FFFFFF',
+      });
     }
-    if(gameState[itemNumber] === 'empty'){
-      gameState[itemNumber] = isCross ? 'cross' : 'circle'
-      setIsCross (!isCross)
-    }
-    else{
+    if (gameState[itemNumber] === 'empty') {
+      gameState[itemNumber] = isCross ? 'cross' : 'circle';
+      setIsCross(!isCross);
+    } else {
       return Snackbar.show({
-        text:'Position is filled',
-        backgroundColor:'red',
-        textColor:'#FFF'
-      })
+        text: 'Position is filled',
+        backgroundColor: 'red',
+        textColor: '#FFF',
+      });
     }
-  }
-  
-  checkIsWinner()
+  };
+
+  checkIsWinner();
 
   return (
-    <SafeAreaView >
-      <StatusBar
-      
-      />
-      <View>
-        <Text> Tic Tac Toe With Shubham 😁</Text>
-      </View>
-     
-    </SafeAreaView>
+    <>
+      <SafeAreaView>
+        <StatusBar />
+        {gameWinner ? (
+          <View style={[styles.playerInfo, styles.winnerInfo]}>
+            <Text style={styles.winnerText}>{gameWinner}</Text>
+          </View>
+        ) : (
+          <View
+            style={[
+              styles.playerInfo,
+              isCross ? styles.PlayerX : styles.Player0,
+            ]}>
+            <Text style={styles.gameTurnText}>
+              Player {isCross ? 'X' : '0'} 's Turn
+            </Text>
+          </View>
+        )}
+
+        {/* Game Grid */}
+        <FlatList
+          numColumns={3}
+          data={gameState}
+          style={styles.grid}
+          renderItem={({item, index}) => (
+            <Pressable
+              key={index}
+              style={styles.card}
+              onPress={() => {
+                onChangeItem(index);
+              }}>
+              <Icons name={item} />
+            </Pressable>
+          )}
+        />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  playerInfo: {},
+  winnerInfo: {},
+  winnerText: {},
+  PlayerX: {},
+  Player0: {},
+  gameTurnText: {},
+  grid: {},
+  card: {},
 });
 
 export default App;
