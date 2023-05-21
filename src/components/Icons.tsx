887a16410e7cf4d1f4b106c435427635a2d542a4
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-05-21 09:58:26 +0530

    types/vectos

---
+++ src/components/Icons.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import type { PropsWithChildren } from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome'
+
+type IconsProps = PropsWithChildren<{
+    name:string
+}>
+
+
+ Icons() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Icons;
